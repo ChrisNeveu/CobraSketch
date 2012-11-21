5386a4a8923b28929cc6d6676d409e62d0c0fd80
mcaloon001 <mcaloon001@connect.wcsu.edu>
2012-11-20 21:05:36 -0500

    Set up the basic structure for the stroke class

--- stroke.py
+++ stroke.py
@@ -2,4 +2,18 @@
 #by Andrew Sheldon, Chris Neveu, Ryan Darge, and Collin McAloon
 
 class Stroke:
+    #Temp variables 
+    x_value = 10
+    y_value = 14
+    shade = 0
+    flag = False
     
+    stroke = (x_value, y_value, shade)
+
+    #Toggles the visibility of the stroke
+    def setVisibility(hidden):
+        Stroke.flag = hidden
+
+    #Gets the current visibility
+    def getVisibility():
+        return Stroke.flag
