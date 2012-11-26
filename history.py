3a67c42206072c49f6c09a5d4c4f203c4a3a565e
mcaloon001 <mcaloon001@connect.wcsu.edu>
2012-11-26 15:57:31 -0500

    Added self and if statements to history

--- history.py
+++ history.py
@@ -1,33 +1,36 @@
 #CobraSketch
 #by Andrew Sheldon, Chris Neveu, Ryan Darge, and Collin McAloon
-'''from .stroke import Stroke'''
-'''from .action import Action'''
+from stroke import Stroke
+from action import Action
 
 class History:
     '''Set up basic variables to see if it would run'''
     index = []
     x = 0
     #undo an action
-    def undoAction(undo):
-        #History.index[undo]'''Relate to Stroke Class'''
-        undo.setVisibility(False)
+    def undoAction(self, undo):
+        if (issubclass(type(undo), Stroke)):
+            undo.setVisibility(False)
     #redo an Action
-    def redoAction(redo):
-        #History.index[redo]'''Relate to Stroke Class'''
-        redo.setVisibility(True)
+    def redoAction(self, redo):
+        if (issubclass(type(redo), Stroke)):
+            redo.setVisibility(True)
     #get list of history action
-    def getHistory():
-        return History.index
+    def getHistory(self):
+        return self.index
     #add an action to history
-    def addAction(Action):
-        if (Action):
-            History.index.append(Action.stroke)
-        #History.index.append(Action)
-        History.x = History.x + 1
+    def addAction(self, Action):
+        if (issubclass(type(Action), Stroke)):
+            self.index.append(Action.stroke)
+        if (issubclass(type(Action), Brush)):
+            self.index.append(Action.brush)
+        if (issubclass(type(Action), Layer)):
+            self.index.append(Action.layer)
+        self.x = self.x + 1
         return Action    
     #get the last action saved to history 
-    def lastAction():
-        return History.index[History.x - 1]
+    def lastAction(self):   
+        return self.index[self.x - 1]
 
 
     
