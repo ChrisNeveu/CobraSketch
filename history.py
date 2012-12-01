f1a1fbe4a4a4ad56c5719e7c243df17a8015e75d
mcaloon001 <mcaloon001@connect.wcsu.edu>
2012-12-01 15:56:42 -0500

    Added undo and redo for layer, took commented out implementation and moved it to bottom of the program

--- history.py
+++ history.py
@@ -24,16 +24,16 @@ class History:
         '''Sets visibility of an object to false'''
         if (undo.Stroke.vis == True):
             undo.Stroke.setVisibility(False)
-       # if (undo.Layer.vis == True):
-       #     undo.Layer.vis = False
+        if (undo.Layer.visible == True):
+            undo.Layer.visible = False
             
     #redo an Action
     def redoAction(self, redo):
         '''Sets visibility of an object to True'''
         if (redo.Stroke.vis == False):
-              redo.Stroke.setVisibility(True)
-      # if (redo.Layer.vis == False):
-       #     redo.Layer.vis = True
+            redo.Stroke.setVisibility(True)
+        if (redo.Layer.visible == False):
+            redo.Layer.visible = True
                 
     def getHistory(self):
         '''get list of history action'''
@@ -48,8 +48,37 @@ class History:
         self.index.append(Act)
         self.index2.append((Act.name, self.x))
         self.x = self.x + 1
-        #Old implementation 
-        '''if (issubclass(type(Act), Stroke)):
+        
+    #get the last action saved to history 
+    def lastAction(self):
+        '''returns last variable saved to index'''
+        return self.index2[self.x - 1]
+
+#History test main
+'''
+h = History()
+x = Stroke([(2,3),(5,1),(2,3),(5,3)],6)
+y = Stroke((5,3), 5)
+
+layer = Layer('newLayer', 2, 5, "lulz")
+
+newAction = Action()
+newAction.name = 'Stroke'
+act1 = Action()
+act1.name = layer.name
+act1.visible = layer.visible
+newAction.Stroke = x
+act1.Layer = layer
+actStroke = Action
+actStroke.Stroke = y
+actStroke.name = 'Stroke '
+h.addAction(act1)
+h.addAction(newAction)
+h.addAction(actStroke)
+h.undoAction(h.index[2])
+
+#addAction Old implementation 
+        if (issubclass(type(Act), Stroke)):
             self.index.append(Act)
             self.index2.append(('Stroke', self.x))
         #if (issubclass(type(Act), Brush)):
@@ -60,32 +89,7 @@ class History:
             self.index.append(Act)
             #need Layer variable
             self.index2.append((Act.name, self.x))
-        self.x = self.x + 1    '''
-        
-    #get the last action saved to history 
-    def lastAction(self):
-        '''returns last variable saved to index'''
-        return self.index2[self.x - 1]
-
-#History test main
-h = History()
-x = Stroke([(2,3),(5,1),(2,3),(5,3)],6)
-y = Stroke((5,3), 5)
-#layer = Layer('newLayer', 2, 5,"lulz")
-'''
-newAction = Action()
-newAction.name = 'Stroke'
-act1 = Action()
-act1.name = layer.name
-newAction.Stroke = x
-act1.Layer = layer
-actStroke = Action
-actStroke.Stroke = y
-actStroke.name = 'Stroke '
-h.addAction(act1)
-h.addAction(newAction)
-h.addAction(actStroke)
-h.undoAction(h.index[2])
+        self.x = self.x + 1
 '''
 
 
