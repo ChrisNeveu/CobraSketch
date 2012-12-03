f90e4cd1347de0ab67cd0bd30331dde120eb449e
mcaloon001 <mcaloon001@connect.wcsu.edu>
2012-12-03 16:58:36 -0500

    Added documentation to texture

--- texture.py
+++ texture.py
@@ -5,9 +5,11 @@ class Texture:
     '''Class for the textures for drawing'''
 
     def __init__(self, center, points):
-            self.center = center
-            self.points = points
+        '''Texture Constructor'''
+        self.center = center
+        self.points = points
         
     def brush(self):
+        '''returns the points for the brush'''
         return self.points
     
