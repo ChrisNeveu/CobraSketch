0c99a63715ab6a5f2408f46cb1b3310151b0f281
ChrisNeveu <mail@chrisneveu.com>
2012-12-06 11:54:50 -0500

    Further Increased Dialog priority

--- cobraSketch.py
+++ cobraSketch.py
@@ -44,7 +44,7 @@ class CobraSketch:
             "font_size": 10
         })   
         self.bg_group = pyglet.graphics.OrderedGroup(0)
-        self.fg_group = pyglet.graphics.OrderedGroup(2)
+        self.fg_group = pyglet.graphics.OrderedGroup(500)
 
         #Create GUI
         self.mainDialog()
